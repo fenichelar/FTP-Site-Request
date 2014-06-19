7e4641bfcf2e38f8b1c56cdb62a1df5efc314162
Alec Fenichel <fenichelar@gatech.edu>
2014-06-19 12:23:54 -0400

    Record data to csv

--- php/submit.php
+++ php/submit.php
@@ -16,7 +16,7 @@ $password = generatePassword(12);
 
 list($year, $month, $day) = split('[-]', $date);
 $expiration = $month."-".$day."-".$year; 
-$command = "cmd.exe /c C:\inetpub\wwwroot\adminSetupFTP.lnk ".$username." ".$password." ".$expiration;
+$command = "cmd.exe /c C:\inetpub\wwwroot\adminSetupFTP.lnk ".$username." ".$password." ".$expiration." ".$mail_to;
 $output = shell_exec($command);
 
 if($output!="The command completed successfully.\n\n") {
--- setupFTP.bat
+++ setupFTP.bat
@@ -1,3 +1,4 @@
 @echo off
 net user %1 %2 /expire:%3 /add
 mkdir F:\FTP\LocalUser\%1
+echo %1,%2,%3,%4>>C:\Users.csv
