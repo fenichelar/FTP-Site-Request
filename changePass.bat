89109afe0799f4371125866a95cba620d4fbad51
Alec Fenichel <fenichelar@gatech.edu>
2014-06-20 15:41:44 -0400

    Use full path for user records csv

--- changePass.bat
+++ changePass.bat
@@ -1,8 +1,8 @@
 @echo off &setlocal
 net user %1 /delete
 if NOT errorlevel 1 (
-	find /V "%1" Users.csv > temp.csv
-	more +2 temp.csv > Users.csv
+	find /V "%1" C:\Users.csv > C:\temp.csv
+	more +2 C:\temp.csv > C:\Users.csv
 	del C:\temp.csv
 	net user %1 %2 /expire:%3 /add
 	if NOT errorlevel 1 (
